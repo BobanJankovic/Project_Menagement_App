e9c49b7372f1cfe1c3df6216235f2dc399bbf14b
bobi <boban0092@gmail.com>
2018-08-13 23:28:12 +0200

    Tanant doesnt exist

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,7 +1,9 @@
 class Tenant < ApplicationRecord
 
-   acts_as_universal_and_determines_tenant
+  acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
+
+  
   validates_presence_of :name
   validates_uniqueness_of :name
 
