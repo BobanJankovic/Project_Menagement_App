f7290e4f462bcd503186142fe4f07edf557a5917
bobi <boban0092@gmail.com>
2018-08-15 11:44:47 +0200

    Invite new members ,confirmation mail

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,17 @@ Rails.application.routes.draw do
     
   # *MUST* come *BEFORE* devise's definitions (below)
   as :user do   
-    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+    match 'user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
 
   devise_for :users, :controllers => { 
     :registrations => "milia/registrations",
-    :confirmations => "confirmations",
+    :confirmations => "milia/confirmations",
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
   }
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
